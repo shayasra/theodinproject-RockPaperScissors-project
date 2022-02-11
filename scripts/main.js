4b7d53c7d113c7460bd4c19d7d8fdb831fb3530c
shayasra <shayasra@gmail.com>
2022-02-11 13:24:45 +0300

    Project Part 1 compleated

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay() {
     let choices = ['ROCK', 'PAPER', 'SCISSORS'];
     let randomIndex = Math.floor(Math.random() * 3);
@@ -27,33 +30,39 @@ function consoleMsg(playerSelection, computerSelection) {
 
 
 function playRound(ps, cs) {
-    // here ps = playerSElection & cs = computer Selection
+    // here ps = playerSelection & cs = computer Selection
     if (ps === cs) {
         consoleMsg(ps, cs);
         return "It's a TIE!";
     }
     else if (ps === 'ROCK' && cs === "PAPER") {
         consoleMsg(ps, cs);
+        computerScore += 1;
         return "Player Loose | Computer Win";
     }
     else if (ps === "ROCK" && cs === "SCISSORS") {
         consoleMsg(ps, cs);
+        playerScore += 1;
         return "Player Win | Computer Loose";
     }
     else if (ps === "PAPER" && cs === "ROCK") {
         consoleMsg(ps, cs);
+        playerScore += 1;
         return "Player Win | Computer Loose";
     }
     else if (ps === "PAPER" && cs === "SCISSORS") {
         consoleMsg(ps, cs);
+        computerScore += 1;
         return "Player Loose | Computer Win";
     }
     else if (ps === "SCISSORS" && cs === "ROCK") {
         consoleMsg(ps, cs);
+        computerScore += 1;
         return "Player Loose | Computer Win";
     }
     else if (ps === "SCISSORS" && cs === "PAPER") {
         consoleMsg(ps, cs);
+        playerScore += 1;
         return "Player Win | Computer Loose";
     }
 }
@@ -62,4 +71,15 @@ function playRound(ps, cs) {
 for (let i = 0; i < 5; i++) {
     console.log(playRound(playerPlay(), computerPlay()));
 }
+
+
 console.log("Winner is .....")
+if (playerScore > computerScore){
+    console.log('Player is the WINNER and Scores is ' + playerScore + " | Computer Scores only: " + computerScore);
+}
+else if (playerScore === computerScore){
+    console.log("TIE!!!!!!!!!!!!" + " Computer Score is: " + computerScore + " | Player Scores is: " + playerScore);
+}
+else {
+    console.log('Computer is the WINNER and Scores is ' + computerScore + " | Player Scores only: " + playerScore);
+}
